74574bc56c19cf6e60aa105f7999800a2058305c
ewno92 <77653755+ewno92@users.noreply.github.com>
2021-07-03 19:06:29 -0700

    client hierarchy created

---
+++ client/components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "./Post/Post.js";
+const Posts = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/Home.scss";
+
+// import Posts from "../components/Posts/Posts.js";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
